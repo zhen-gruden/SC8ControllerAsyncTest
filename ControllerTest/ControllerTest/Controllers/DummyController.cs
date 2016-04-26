3741f29684b41fa80480d9cd2e97ddf3e2b4302b
Peng Song <peng@gruden.com>
2016-04-26 17:05:05 +1000

    Initial commit

---
+++ ControllerTest/ControllerTest/Controllers/DummyController.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerTest.Controllers
+{
+    public class DummyController : SitecoreController
+    {
+        // GET: Dummy
+        public async Task<ActionResult> Test()
+        {
+            // download a bunch of URLs in parallel with await
+            var webClient = new WebClient();
+
+            var urls = new[] {
+                "https://google.com",
+                "https://bing.com",
+                "https://yahoo.com"
+            }.Select(url => webClient.DownloadStringTaskAsync(url));
+
+            var contents = await Task.WhenAll(urls);
+
+            // or just await one task
+            var google = await webClient.DownloadStringTaskAsync("https://google.com");
+
+            // execution will pick up right here when all the awaited tasks are done - thawing the thread to finish execution
+
+            return View(contents);
+        }
+    }
+}
